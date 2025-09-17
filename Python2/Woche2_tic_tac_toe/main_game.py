089095ee47860af374caf38a76f454c7fd7cae59
Mengni Zhang <monicamengni@gmail.com>
2025-09-17 15:44:39 +0200

    refactor play_single_game() to reduce duplicate codes, by using player_symbols variable

--- Python2/Woche2_tic_tac_toe/main_game.py
+++ Python2/Woche2_tic_tac_toe/main_game.py
@@ -9,16 +9,16 @@ from functions import (
     win_check,
 )
 
+VALID_CELLS = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+
 
 def play_single_game():
     # Set up the data structure
     # Board is a 3x3 grid. it's data structure is a dictionary.
     # The dictionary keys are the name of 9 squares from A1 - C3.
-    valid_cells = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
-
     # Before each square is taken, the square's name will be shown,
     # so it's easy for the players to choose.
-    board = {cell: f"({cell})" for cell in valid_cells}
+    board = {cell: f"({cell})" for cell in VALID_CELLS}
 
     game_on = True
 
@@ -26,40 +26,24 @@ def play_single_game():
     print("Yaaay! Let's play!")
 
     players = player_order()
-    player_symbols = {players[0]: PLAYER_X, players[1]: PLAYER_O}
+    player_symbols = {players[0]: PLAYER_O, players[1]: PLAYER_X}
     turn = players[0]
     display_board(board)
 
     while game_on:
-        if turn == players[0]:
-            player_choice = choose_square(turn, board, valid_cells)
-            board[player_choice] = PLAYER_X
-            display_board(board)
+        player_choice = choose_square(turn, board, VALID_CELLS)
+        board[player_choice] = player_symbols[turn]
+        display_board(board)
 
-            if win_check(board):
-                game_on = False
-                print(f"\n{turn} has won the game!")
-            elif is_full_board(board):
-                game_on = False
-                print("\nBoard is full. It's a draw.")
-            else:
-                turn = players[1]
-                print(f"\nNow it's {players[1]}'s turn.")
-
+        if win_check(board):
+            game_on = False
+            print(f"\n{turn} has won the game!")
+        elif is_full_board(board):
+            game_on = False
+            print("\nBoard is full. It's a draw.")
         else:
-            player_choice = choose_square(turn, board, valid_cells)
-            board[player_choice] = PLAYER_O
-            display_board(board)
-
-            if win_check(board):
-                game_on = False
-                print(f"\n{turn} has won the game!")
-            elif is_full_board(board):
-                game_on = False
-                print("\nBoard is full. It's a draw.")
-            else:
-                turn = players[0]
-                print(f"\nNow it's {players[0]}'s turn.")
+            turn = players[1] if turn == players[0] else players[0]
+            print(f"\nNow it's {turn}'s turn.")
 
 
 def main():
